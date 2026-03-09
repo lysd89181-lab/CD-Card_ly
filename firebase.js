20f297c043d5d3b426511cdba88be0ff39eeb45e
lysd89181-lab <lysd89181@gmail.com>
2026-03-10 00:12:35 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,18 @@
-// Firebase Config
+// إعداد Firebase
+
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT.firebaseapp.com",
-  projectId: "PROJECT",
-  storageBucket: "PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyARNJOPAycWU3YJdZSBN1d9uoLrvso3i4Y",
+  authDomain: "cd-card-87365.firebaseapp.com",
+  projectId: "cd-card-87365",
+  storageBucket: "cd-card-87365.firebasestorage.app",
+  messagingSenderId: "397926299412",
+  appId: "1:397926299412:web:efbe9ae9aae29df368f401",
+  measurementId: "G-NDL36M3W68"
 };
 
+// تشغيل Firebase
 firebase.initializeApp(firebaseConfig);
 
+// تشغيل الخدمات
 const auth = firebase.auth();
 const db = firebase.firestore();
